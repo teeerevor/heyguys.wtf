70e61969ecb936a2926f43cd29ff082d9341a15a
trev <trev@trevs-MacBook-Pro.local>
2019-01-01 21:42:52 +1100

    some updates

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
-import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
+import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -9,7 +9,7 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 // import Header from './header'
-        // <Header siteTitle={data.site.siteMetadata.title} />
+// <Header siteTitle={data.site.siteMetadata.title} />
 
 const Flex = styled.div`
     padding: 0;
@@ -18,35 +18,34 @@ const Flex = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #FBAB7E;
-    background-image: linear-gradient(62deg, #FBAB7E 0%, #F7CE68 100%);
+    background-color: #fbab7e;
+    background-image: linear-gradient(62deg, #fbab7e 0%, #f7ce68 100%);
 `;
+
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-        <ThemeProvider theme={{ fontFamily: 'Helvetica Neue' }}>
-      <>
-      <GlobalStyle/>
-    <Flex>
-          {children}
-    </Flex>
-      </>
-        </ThemeProvider>
-    )}
-  />
-)
+    <StaticQuery
+        query={graphql`
+            query SiteTitleQuery {
+                site {
+                    siteMetadata {
+                        title
+                    }
+                }
+            }
+        `}
+        render={data => (
+            <ThemeProvider theme={{ fontFamily: 'Helvetica Neue' }}>
+                <>
+                    <GlobalStyle />
+                    <Flex>{children}</Flex>
+                </>
+            </ThemeProvider>
+        )}
+    />
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+    children: PropTypes.node.isRequired,
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import styled from 'styled-components'
+import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import logo from '../images/heyguys-white.svg'
-import SEO from '../components/seo'
+import Layout from '../components/layout';
+import logo from '../images/heyguys-white.svg';
+import SEO from '../components/seo';
 
 const HiddenH1 = styled.h1`
     opacity: 0;
@@ -18,11 +17,11 @@ const Logo = styled.img`
 `;
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Hey Guys WTF" keywords={[`hey`, `guys`, `what`, `the`, `fuck`]} />
-    <HiddenH1>Hey Guys WTF</HiddenH1>
-    <Logo src={logo} alt="Hey Guys WTF" />
-  </Layout>
-)
+    <Layout>
+        <SEO title="HeyGuys.wtf!" keywords={[`hey`, `guys`, `what`, `the`, `fuck`]} />
+        <HiddenH1>Hey Guys WTF</HiddenH1>
+        <Logo src={logo} alt="Hey Guys WTF" />
+    </Layout>
+);
 
-export default IndexPage
+export default IndexPage;
